d99716af317d57a3615ffccc6969d8c30b2f7b5d
matthew <matthewjaestokeley@gmail.com>
2018-12-20 16:57:20 +0000

    remove element property

---
+++ src/js/Label.js
@@ -0,0 +1,65 @@
+class Labels {
+    constructor (options) {
+
+        this.store = options.store;
+        this.collectionManager = options.collectionManager;
+        this.container = options.container;
+        this.name = options.name;
+        this.method = options.method;
+        this.requestId = options.requestId;
+        this.init();
+
+    }
+
+    init () {
+        this.setTemplate();
+        this.setId(this.formatId(this.name)); 
+        events.register('response', (res, id) => {
+          if (id === this.requestId) {
+           this.container.innerHTML = this.renderTemplate(res);
+          }
+        })
+    }
+
+    /**
+     * [setTemplate description]
+     */
+    setTemplate () { 
+        this.template = templates[this.name];
+        return this;
+    }
+
+    /**
+     * [Template description]
+     */
+    getTemplate () {
+        return this.template;
+    }
+
+    formatId (id) { return encodeURI(id); }
+
+    setId (id) { this.id = id; return this; }
+
+    getId () { return this.id; }
+
+    renderTemplate (data) {
+        return this.template(data);
+    }
+
+    render () {
+        var data = this.collectionManager[this.method]();
+        this.container.innerHTML = this.renderTemplate(data);
+        return this;
+    }
+
+    request() {
+      this.collectionManager[this.method]();
+      return this;
+    }
+
+    renderAsync(data) {
+        this.container.innerHTML = this.renderTemplate(data);
+        return this;
+    }
+
+}
